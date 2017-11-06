ebce82fbf9aa3345cee1deedb362ae9c48e8c69c
NazarPonochevnyi <nazar.ponochevnyi@gmail.com>
2017-11-06 20:06:27 +0200

    Fixed bugs, add possibility input data.

--- Java/one.java
+++ Java/one.java
@@ -1,53 +1,68 @@
 package systems_of_calculus;
 
-/*
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-*/
+import java.util.Scanner;
+
 
 public class one {
 	
+	private static Scanner scanner;
+
+
 	public static int start_10(String num, int n) {
 		return trek_10(num, num.length() - 1, n, 0, 0);
 	}
 	
 	public static int trek_10(String num, int len_n, int n, int i, int result) {
-		result += Integer.valueOf(num.charAt(i)) * Math.pow(n, len_n - i);
+		int symbol = Character.getNumericValue(num.charAt(i));
+		result += symbol * Math.pow(n, len_n - i);
 		if (i != len_n) { return trek_10(num, len_n, n, i + 1, result); }
 		return result;
 	}
 	
-	public static String start_n(int num, int n) {
+	
+	public static int start_n(int num, int n) {
 		return trek_n(num, n, "");
 	}
 	
-	public static String trek_n(int num, int n, String result) {
+	public static int trek_n(int num, int n, String result) {
 		int div = num / n;
 		int mod = num % n;
 		if (num != 0) { return trek_n(div, n, String.valueOf(mod) + result); }
-		return result;
+		if (result == "") { result = "0"; }
+		return Integer.parseInt(result);
 	}
 	
 
 	public static void main(String[] args) {
-		int num1 = start_10("10".trim(), Integer.parseInt("10"));
-		int num2 = start_10("10".trim(), Integer.parseInt("10"));
+		scanner = new Scanner(System.in);
 		
-		/*
-		Map unmodifiableMap = Collections.unmodifiableMap(new HashMap(2, 1.0f){
-	        {
-	            put("+", num1 + num2);
-	            put("-", num1 - num2);
-	           	put("*", num1 * num2);
-	           	put("/", num1 / num2);
-	        }
-	    });
-		final Map make = unmodifiableMap;
-		*/
+		int num1, num2, res, result;
+		String f_num1, f_num2, action;
 		
-		String result = start_n(num1 + num2, Integer.parseInt("10".trim()));
-		System.out.println(result);
+		System.out.print("\nEnter first number: ");
+		f_num1 = scanner.nextLine().trim();
+		System.out.print("Enter system of calculus for first number: ");
+		num1 = start_10(f_num1, Integer.parseInt(scanner.nextLine().trim()));
+		
+		System.out.print("\nEnter second number: ");
+		f_num2 = scanner.nextLine().trim();
+		System.out.print("Enter system of calculus for second number: ");
+		num2 = start_10(f_num2, Integer.parseInt(scanner.nextLine().trim()));
+		
+		System.out.print("\nEnter action for numbers: ");
+		action = scanner.nextLine().trim();
+		
+		switch (action) {
+			case ("+"): { res = num1 + num2; break; }
+			case ("-"): { res = num1 - num2; break; }
+			case ("*"): { res = num1 * num2; break; }
+			case ("/"): { res = num1 / num2; break; }
+			default: { res = 0; System.out.println("Unknown command!"); break; }
+		}
+		
+		System.out.print("Enter system of calculus for answer: ");
+		result = start_n(res, Integer.parseInt(scanner.nextLine().trim()));
+		
+		System.out.println("\nResult: " + String.valueOf(result));
 	}
-
 }
